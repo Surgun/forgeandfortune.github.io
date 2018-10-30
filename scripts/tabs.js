92a7a9fb8d5ad5bf13ffee5a1ed9392ebc014e55
akerson <pirodaggar@gmail.com>
2018-10-30 09:46:55 -0400

    hotkeys for tabs

--- scripts/tabs.js
+++ scripts/tabs.js
@@ -18,32 +18,43 @@ function openTab(tabName) {
     $("#"+tabName).show();
 }
 
+function tabClick(e, name) {
+    openTab(name);
+    navTabHighlight(e,$('#'+name+'Link')[0]);
+}
+
 const $comptitle1 = $("#comptitle1");
 const $comptitle2 = $("#comptitle2");
 const $comptitle3 = $("#comptitle3");
 
 $comptitle1.click((e) => {
     e.preventDefault();
-    openTab("inventoryTab");
-    navTabHighlight(e,$('#inventoryTabLink')[0]);
+
 });
 
 $comptitle2.click((e) => {
     e.preventDefault();
-    openTab("recipesTab");
-    navTabHighlight(e,$('#recipeTab')[0]);
+    tabClick(e, "recipesTab");
 });
 
 $comptitle3.click((e) => {
     e.preventDefault();
-    openTab("dungeonsTab");
-    navTabHighlight(e,$('#dungeonsTabLink')[0]);
+    tabClick(e, "dungeonsTab");
 });
 
 $(document).on('click', "#DungeonSideBarStatus", (e) => {
     e.preventDefault();
-    openTab("dungeonsTab");
-    navTabHighlight(e,$('#dungeonsTabLink')[0]);
+    tabClick(e, "dungeonsTab");
     const dungeonID = $(e.currentTarget).attr("dungeonID");
     showDungeon(dungeonID);
 })
+
+$(document).on( "keypress", (e) => {
+    if (e.which === 49) tabClick(e, "inventoryTab");
+    else if (e.which === 50) tabClick(e, "recipesTab");
+    else if (e.which === 51) tabClick(e, "workersTab");
+    else if (e.which === 52) tabClick(e, "heroesTab");
+    else if (e.which === 53) tabClick(e, "dungeonsTab");
+    else if (e.which === 54) tabClick(e, "eventsTab");
+    else if (e.which === 55) tabClick(e, "progressTab");
+});

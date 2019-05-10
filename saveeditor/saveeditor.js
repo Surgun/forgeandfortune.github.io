4a5a843cb32448b484a3e749b352024cf3eb7673
akerson <pirodaggar@gmail.com>
2019-05-10 11:47:30 -0400

    save editor done

--- saveeditor/saveeditor.js
+++ saveeditor/saveeditor.js
@@ -5,26 +5,31 @@ const saveBox = document.getElementById("save");
 saveBox.setAttribute("value",saveFile);
 
 const Heroes = [];
+const Recipes = [];
 
 $.ajax({
     url: "../json/heroes.json",
-}).done((data) => {
+}).done(data => {
     console.log("hero load complete");
     $.each(data, function(i,props){
         Heroes.push(props);
     });
 });
 
-function ExportSave() {
-    const saveFile = createSaveExport();
-    $("#exportSaveText").val(saveFile);
-    ga('send', 'event', 'Save', 'export', 'export');
-}
+$.ajax({
+    url: "../json/recipes.json",
+}).done(data => {
+    console.log("recipe load complete");
+    $.each(data, function(i,props){
+        Recipes.push(props);
+    });
+});
 
 function ImportSaveButton() {
     localStorage.setItem('saveeditor', saveBox.value);
-    const unpako = atob(document.getElementById("save").value);
-    unpackedSave = JSON.parse(JSON.parse(pako.ungzip(unpako,{ to: 'string' })));
+    const pakostring = atob(document.getElementById("save").value);
+    const unpako = pako.ungzip(pakostring,{ to: 'string' });
+    unpackedSave = JSON.parse(JSON.parse(unpako));
     refreshInputs();
 }
 
@@ -44,17 +49,53 @@ function refreshInputs() {
             $("<div/>").addClass('heroName').html(idToHero[hero.id]).appendTo(d);
             for (let i=1;i<7;i++) {
                 const s = $("<div/>").addClass('slot').appendTo(d)
-                    $("<button/>").addClass('slotType').text('TYPE').data("hid",hero.id).data("type",`slot${i}Type`).appendTo(s);
-                    $("<button/>").addClass('slotTier').text('Tier 1').data("hid",hero.id).appendTo(s);
-                    $("<button/>").addClass('slotRarity').text('Common').data("hid",hero.id).appendTo(s);
-                    $("<button/>").addClass('slotSharp').text('+0').data("hid",hero.id).appendTo(s);
+                    $("<button/>").addClass('slotType').attr("id",hero.id+"type"+i).text(getType(hero,i)).data("hid",hero.id).data("type",`slot${i}Type`).appendTo(s);
+                    $("<button/>").addClass('slotRarity').attr("id",hero.id+"rarity"+i).text(getRarity(hero,i)).data("hid",hero.id).appendTo(s);
+                    $("<button/>").addClass('slotTier').attr("id",hero.id+"tier"+i).text(getTier(hero,i)).data("hid",hero.id).appendTo(s);
+                    $("<button/>").addClass('slotSharp').attr("id",hero.id+"sharp"+i).text(getSharp(hero,i)).data("hid",hero.id).appendTo(s);
             };
     });
 }
 
 const Tier = ["","Tier 1","Tier 2","Tier 3","Tier 4","Tier 5","Tier 6","Tier 7", "Tier 8", "Tier 9", "Tier 10"]
-const Rarity = ["Common","Good","Great","Epic"];
+const Rarity = ["Common","Good","Rare","Epic"];
 const Sharp = ["+0","+1","+2","+3","+4","+5","+6","+7","+8","+9","+10"];
+
+function getType(hero,i) {
+    const slot = hero["slot"+i];
+    if (slot === null) return hero[`slot${i}Type`][0];
+    return Recipes.find(r=>r.id===slot.id).type;
+}
+
+function getRarity(hero,i) {
+    const slot = hero["slot"+i];
+    if (slot === null) return Rarity[0];
+    return Rarity[slot.rarity];
+}
+
+function getTier(hero,i) {
+    const slot = hero["slot"+i];
+    if (slot === null) return Tier[1];
+    return Tier[Recipes.find(r=>r.id===slot.id).lvl];
+}
+
+function getSharp(hero,i) {
+    const slot = hero["slot"+i];
+    if (slot === null) return Sharp[0];
+    return Sharp[slot.sharp];  
+}
+
+function allRarity(rarity) {
+    $(".slotRarity").text(rarity);
+}
+
+function allTier(tier) {
+    $(".slotTier").text("Tier "+tier)
+}
+
+function allSharp(sharp) {
+    $(".slotSharp").text("+"+sharp);
+}
 
 $(document).on("click",".slotType",(e) => {
     e.preventDefault();
@@ -63,7 +104,6 @@ $(document).on("click",".slotType",(e) => {
     const slot = d.data("type");
     const currentTarget = d.text();
     const hero = Heroes.find(h=>h.id === hid);
-    console.log(slot,hero[slot])
     let i = hero[slot].indexOf(currentTarget)+1;
     if (hero[slot].length === i) i = 0;
     d.text(hero[slot][i]);
@@ -96,10 +136,6 @@ $(document).on("click",".slotSharp",(e) => {
     d.text(Sharp[lvl]);
 });
 
-function cycleType(id,slot,type) {
-    return;
-}
-
 const idToHero = {
     "H001":"Beorn",
     "H002":"Cedric",
@@ -113,4 +149,40 @@ const idToHero = {
     "H202":"Grogmar",
     "H203":"Revere",
     "H204":"Caeda",
+}
+
+function ExportSave() {
+    unpackedSave["h"].forEach(hero => {
+        hero.slot1 = makeItem(hero.id,1);
+        hero.slot2 = makeItem(hero.id,2);
+        hero.slot3 = makeItem(hero.id,3);
+        hero.slot4 = makeItem(hero.id,4);
+        hero.slot5 = makeItem(hero.id,5);
+        hero.slot6 = makeItem(hero.id,6);
+    });
+    const pakoSave = pako.gzip(JSON.stringify(JSON.stringify(unpackedSave)),{ to: 'string' });
+    saveFile = btoa(pakoSave);
+    saveBox.setAttribute("value",saveFile);
+    localStorage.setItem('saveeditor', saveBox.value);
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(saveBox).val()).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
+
+function makeItem(hid,slot) {
+    const typeText = $(`#${hid}type${slot}`).text();
+    const tierText = $(`#${hid}tier${slot}`).text();
+    const tierFix = Tier.indexOf(tierText);
+    const rarityText = $(`#${hid}rarity${slot}`).text();
+    const sharpText = $(`#${hid}sharp${slot}`).text();
+    const recipeId = Recipes.find(r => r.type === typeText && r.lvl === tierFix).id;
+
+    const item = {};
+    item.id = recipeId;
+    item.rarity = Rarity.indexOf(rarityText);
+    item.seed = 1337
+    item.sharp = Sharp.indexOf(sharpText);
+    return item;
 }

549a6adc4e22fe0b93572102453a08100b40129a
akerson <pirodaggar@gmail.com>
2019-06-04 17:00:18 -0400

    less letters

--- scripts/dungeons.js
+++ scripts/dungeons.js
@@ -142,12 +142,13 @@ class Dungeon {
             h.hp = h.maxHP()
         });
         if (this.type === "boss" && this.mobs.every(m=>m.dead())) {
+            console.log("heyyy");
             EventManager.addEventBoss(this.id,this.dungeonTime);
             DungeonManager.bossesBeat.push(this.id);
             refreshALprogress();
             refreshRecipeFilters();
         }
-        else EventManager.addEventDungeon(this.eventLetter,this.dropList,this.dungeonTotalTime,this.floorCount, this.beatTotal);
+        else if (this.type === "normal") EventManager.addEventDungeon(this.eventLetter,this.dropList,this.dungeonTotalTime,this.floorCount, this.beatTotal);
         DungeonManager.removeDungeon(this.id);
         if (DungeonManager.dungeonView === this.id) {
             BattleLog.clear();

7e14084d624953010be19834e3a13e0c87b0ab28
akerson <pirodaggar@gmail.com>
2019-09-02 15:13:27 -0400

    start of monster hall

--- scripts/monsterhall.js
+++ scripts/monsterhall.js
@@ -1,6 +1,31 @@
 "use strict";
 
-const MonsterHall = {
+const $monsterBuilding = $("#monsterBuilding");
+const $monsterDiv = $(".monsterDiv");
+const $monsterMobs = $("#monsterMobs");
+const $monsterBosses = $("#monsterBosses");
+const $monsterRewards = $("#monsterRewards");
 
+
+const MonsterHall = {
+    lvl : 1,
+    createSave() {
+        const save = {};
+        save.lvl = this.lvl;
+        return save;
+    },
+    loadSave(save) {
+        if (save.lvl !== undefined) this.lvl = save.lvl;
+    },
 }
 
+function intiateMonsterBldg() {
+    $monsterBuilding.show();
+    $monsterDiv.hide().removeClass("selected");
+    $monsterMobs.show().addClass("selected");
+    refreshHallMonsters();
+}
+
+function refreshHallMonsters() {
+    
+}

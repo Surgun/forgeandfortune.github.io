73209f9fdf139490aa7502ab659538f2d7d89094
akerson <pirodaggar@gmail.com>
2019-12-01 22:26:50 -0500

    subtract materials instead of add

--- scripts/shop.js
+++ scripts/shop.js
@@ -24,7 +24,7 @@ const Shop = {
             return;
         }
         ResourceManager.deductMoney(perk.goldCost);
-        ResourceManager.addMaterial(perk.mat,perk.matAmt)
+        ResourceManager.addMaterial(perk.mat,-perk.matAmt)
         this.purchased.push(id);
         perk.activate();
         refreshShop();
